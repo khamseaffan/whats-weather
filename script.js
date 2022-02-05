bc3d3419785b9c63229db5444a33976079e4dc32
Affan <khamseaffan@gmail.com>
2022-02-05 14:31:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+const weatehrInfoCard = document.querySelector(".weather");
+
+let news = {
+  fetchNewsReport: function (searchedCity) {
+    console.log("Fetching Started");
+    let newsUrl = `https://newsapi.org/v2/everything?q=${searchedCity}&sortBy=popularity&apiKey=403af102e6e94d4f899f8d23cf8cb4e3`;
+    fetch(newsUrl)
+      .then((response) => response.json())
+      .then((data) => this.loadNews(data));
+  },
+  loadNews: (data) => {
+    news = "";
+    for (let i = 0; i < 5; i++) {
+      let { title, description, url, urlToImage } = data.articles[i];
+
+      if (title.length > 60) {
+        title = title.substring(0, 55) + "...";
+      }
+      if (description.length > 180) {
+        description = description.substring(0, 178) + "...";
+      }
+
+      news += `<a class="newsTile" href="${url}">
+                <img src="${urlToImage}" alt="" />
+                <div class="content">
+                  <p><span class="title">${title}</span>
+                    <p class="description">${description}</p></p>
+                </div>
+              </a>\n`;
+    }
+    document.querySelector(".newsSection").innerHTML = news;
+  },
+};
+
+let weather = {
+  api_key: "6fabf0a356ff6bd0c36843891fe87692",
+  fetchWeather: function (city) {
+    if (!weatehrInfoCard.classList.contains("loading")) {
+      weatehrInfoCard.classList.add("loading");
+    }
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${this.api_key}`
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp } = data.main;
+    const { speed } = data.wind;
+    const { humidity } = data.main;
+
+    document.querySelector(".city").textContent = `Weather Of ${name}`;
+    document.querySelector(".temp").textContent = `${temp}°C`;
+    document.querySelector(
+      ".icon"
+    ).src = `https://openweathermap.org/img/wn/${icon}.png`;
+    document.querySelector(".description").textContent = description;
+    document.querySelector(".humidity").textContent = `Humidity: ${humidity}%`;
+    document.querySelector(".wind").textContent = `Wind Speed: ${speed} m/sec`;
+    document.body.style.backgroundImage =
+      "url(" + `https://source.unsplash.com/1600x900/?${name}` + ")";
+    weatehrInfoCard.classList.remove("loading");
+  },
+  handelClick: () => {
+    let searchedCity = document.querySelector(".search-bar").value;
+    if (!searchedCity) {
+      console.log("city Value to search not enterrd");
+      return;
+    }
+
+    weather.fetchWeather(searchedCity);
+    news.fetchNewsReport(searchedCity);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.handelClick();
+});
+
+// document.addEventListener("keydown", (event) => {
+//   if (event.key == "Enter") {
+//     weather.handelClick();
+//   }
+// });
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.handelClick();
+    }
+  });
+
+// weather.fetchWeather("california");
